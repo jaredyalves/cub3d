c16017b95b2605e38719229a5457d421dfbfa68e
jcapistr <jcapistr@student.42porto.com>
2024-07-14 19:06:01 +0100

    feat(check): check for multiple paths in textures

--- src/check/check_texture.c
+++ src/check/check_texture.c
@@ -1,4 +1,5 @@
 #include "cub3d.h"
+#include "libft.h"
 
 void	check_texture(char *texture)
 {
@@ -6,4 +7,6 @@ void	check_texture(char *texture)
 		cub3d_exit("Error\nMissing one or more textures\n");
 	if (!has_extension(texture, "xpm"))
 		cub3d_exit("Error\nMissing .xpm extension on one or more textures\n");
+	if (ft_strchr(texture, ' ') || ft_strchr(texture, '\t'))
+		cub3d_exit("Error\nInvalid syntax on one or more textures\n");
 }
