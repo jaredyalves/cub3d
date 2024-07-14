3c3b88ec1eb38d5ed9593117d87aca9be9d92899
jcapistr <jcapistr@student.42porto.com>
2024-07-14 19:25:23 +0100

    feat(check): check for non existing textures

--- src/check/check_texture.c
+++ src/check/check_texture.c
@@ -1,12 +1,24 @@
 #include "cub3d.h"
 #include "libft.h"
 
+#include <fcntl.h>
+#include <unistd.h>
+
 void	check_texture(char *texture)
 {
+	int	fd;
+
 	if (!texture)
 		cub3d_exit("Error\nMissing one or more textures\n");
 	if (!has_extension(texture, "xpm"))
 		cub3d_exit("Error\nMissing .xpm extension on one or more textures\n");
 	if (ft_strchr(texture, ' ') || ft_strchr(texture, '\t'))
 		cub3d_exit("Error\nInvalid syntax on one or more textures\n");
+	fd = open(texture, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("Error\n", 2);
+		cub3d_exit("One or more textures doesn't exist or cannot be opened\n");
+	}
+	close(fd);
 }
