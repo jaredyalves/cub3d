c9416dbebe8883f09289546b2e4462d73b2604e8
jcapistr <jcapistr@student.42porto.com>
2024-07-14 19:16:45 +0100

    feat(check): check for colors values above 255

--- src/check/check_color.c
+++ src/check/check_color.c
@@ -1,15 +1,16 @@
 #include "cub3d.h"
-void check_color(int color[3])
+
+void	check_color(int color[3])
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < 3)
 	{
 		if (color[i] == -1)
 			cub3d_exit("Error\nMissing one or more colors\n");
-		if (color[i] < 0)
-			cub3d_exit("Error\nOne or more colors have negative value\n");
+		if (color[i] < 0 || color[i] > 255)
+			cub3d_exit("Error\nColors must have a value of 0 to 255\n");
 		i++;
 	}
 }
--- src/check/check_map.c
+++ src/check/check_map.c
@@ -1,5 +1,6 @@
 #include "cub3d.h"
-void check_map(char **map, int height)
+
+void	check_map(char **map, int height)
 {
 	(void)map;
 	if (height == 0)
